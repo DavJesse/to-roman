a13b0421a4de6f969b6d1427ea4c9935c6952ecf
davodhiambo <jesseomolo@yahoo.com>
2024-05-24 18:23:44 +0300

    feature: add atoi function

--- stringManipulation.go
+++ stringManipulation.go
@@ -1,5 +1,12 @@
 package main
 
 func atoi(str string) int {
-	var result string
+	var result int
+	var digit int
+
+	for _, r := range str {
+		digit = int(r - '0')
+		result = result * 10 + digit
+	}
+	return result
 } 
--- stringManipulation_test.go
+++ stringManipulation_test.go
@@ -1,4 +1,15 @@
 package main
 
-func testAtoi() {
+import "testing"
+
+func TestAtoi(t *testing.T) {
+	testStr := "123"
+	expected := 123
+	got := atoi(testStr)
+
+	if got != expected {
+		t.Errorf("Got: %d", got)
+		t.Errorf("Expected: %d", expected)
+		t.Errorf("TestAtoi Failed!")
+	}
 }
