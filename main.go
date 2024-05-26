0094c46f9ce7ecbbf02176a810a27bfb441f379e
davodhiambo <jesseomolo@yahoo.com>
2024-05-26 17:30:42 +0300

    main.go: printing roman number, handling imputs of 0

--- main.go
+++ main.go
@@ -16,9 +16,20 @@ func main() {
 		return
 	}
 
-	if num >= 4000 {
-		printLn("Connot convert " + arg + " to Roman numerals")
-		printLn("This program has an input limit of 4000")
+	if num >= 4000 || num == 0{
+		if num >= 4000 {
+			printLn("Connot convert '" + arg + "' to Roman numerals")
+			printLn("This program has an input limit of 4000")
+		} else {
+			printLn("Connot convert '" + arg + "' to Roman numerals")
+			printLn("This program only takes positive integers greater than '0'")
+		}
+		
+		return
 	}
+
+	roman := romanNumeros(num)
+
+	printLn(roman)
 
 }
