4581afd14a0be0e0d86538dd21a1f79be7271206
davodhiambo <jesseomolo@yahoo.com>
2024-05-26 17:07:41 +0300

    feature: implement romanNumeros with struct

--- romanNumeros.go
+++ romanNumeros.go
@@ -1,102 +1,32 @@
 package main
 
+var roman = []struct {
+	symbol string
+	value  int
+}{
+	{"M", 1000},
+	{"CM", 900},
+	{"D", 500},
+	{"CD", 400},
+	{"C", 100},
+	{"XC", 90},
+	{"L", 50},
+	{"XL", 40},
+	{"X", 10},
+	{"IX", 9},
+	{"V", 5},
+	{"IV", 4},
+	{"I", 1},
+}
+
 func romanNumeros(num int) string {
-	var count int
 	var result string
 
-	if num/1000 > 0 {
-		count = num / 1000
-		num /= 1000
-
-		for count > 0 {
-			result += "M"
-			count--
+	for _, val := range roman {
+		for num >= val.value {
+			result += val.symbol
+			num -= val.value
 		}
 	}
-
-	if num/100 > 0 {
-		count = num / 100
-
-		if count == 9 {
-			result += "CM"
-			num -= 900
-			count = 0
-		}
-
-		if count > 5 && count < 9 {
-			result += "D"
-			num -= 500
-			count -= 5
-		}
-
-		for count > 0 {
-			if count == 4 {
-				num -= 400
-				result += "CD"
-				count = 0
-				break
-			}
-
-			num -= 100
-			result += "C"
-			count--
-		}
-	}
-
-	if num/10 > 0 {
-		count /= 10
-
-		if count == 9 {
-			result += "XC"
-			num -= 90
-			count = 0
-		}
-
-		if count > 5 && count < 9 {
-			result += "L"
-			num -= 50
-		}
-
-		for count > 0 {
-			if count == 4 {
-				num -= 40
-				result += "XL"
-				count = 0
-			}
-			result += "X"
-			num -= 10
-			count--
-		}
-	}
-
-	if num/1 > 0 {
-		count = num / 1
-
-		if count == 9 {
-			num -= 9
-			result += "IX"
-			count = 0
-		}
-
-		if count > 5 {
-			count -= 5
-			num -= 5
-			result += "V"
-		}
-
-		for count > 0 {
-			if count == 4 {
-				num -= 4
-				result += "IV"
-				count = 0
-				break
-			}
-
-			num -= 1
-			result += "I"
-			count--
-		}
-	}
-
 	return result
 }
--- roman_numeros_test.go
+++ roman_numeros_test.go
@@ -1,4 +1,15 @@
 package main
 
-func TestRomanNumeros(t *testing) {
+import "testing"
+
+func TestRomanNumeros(t *testing.T) {
+	testInt := 999
+	got := romanNumeros(testInt)
+	expected := "CMXCIX"
+
+	if got != expected {
+		t.Errorf("Got: %s", got)
+		t.Errorf("Expected: %s", expected)
+		t.Errorf("TestRomanNumeros Failed!")
+	}
 }
