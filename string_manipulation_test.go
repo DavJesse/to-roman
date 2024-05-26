3a7527d4ff9557f7b58ee64c8e1416756ee312b3
davodhiambo <jesseomolo@yahoo.com>
2024-05-26 17:56:15 +0300

    Commenting: add comments to string_manipulation_test.go

--- string_manipulation_test.go
+++ string_manipulation_test.go
@@ -2,11 +2,13 @@ package main
 
 import "testing"
 
+// Test atoi function
 func TestAtoi(t *testing.T) {
 	testStr := "123"
 	expected := 123
 	got, err := atoi(testStr)
 
+	//Incase of a fail, print the 'got' and 'expected' variables
 	if err != "" && got != expected {
 		t.Errorf("Got: %d", got)
 		t.Errorf("Expected: %d", expected)
